28f4155b2a41a3915077079b4af755d7ceba141a
Kirill <118915590+Derev005@users.noreply.github.com>
2024-09-29 16:30:00 +0300

    Update main.cpp

--- prac_1/task 2/main.cpp
+++ prac_1/task 2/main.cpp
@@ -4,39 +4,41 @@
 #include <ctime>
 #include <cstdlib>
 #include <chrono>
+
 using namespace std;
 using namespace chrono;
 
 void Task2()
 {
+    int option = 1000;
     const long int size = 10000000;  // Размер std::bitset (10^7 бит)
     bitset<size>* numbers = new bitset<size>;  // Используем std::bitset для хранения битов
 
     long int input;
     ofstream outfile("sortedList.txt");
 
-    cout << "Enter seven digit numbers one by one (0 to complete):\n";
+    cout << "Enter seven digit numbers one by one (0 to complete):" << endl;
     while (true) {
         cin >> input;
         if (input == 0) {
             break;
         }
-        if (input >= size / 10 && input <= 9999999) {
+        if (input >= size / 10 and input <= 9999999) {
             numbers->set(input - 1, 1);
         }
         else {
-            cout << "Only seven digit numbers!\n";
+            cout << "Only seven digit numbers!" << endl;
         }
     }
 
     char spam;
 
-    cout << "Need to enter an additional 1000 seven-digit numbers? (y/n): ";
+    cout << "Need to enter an additional " << option << " seven-digit numbers? (y/n):";
     cin >> spam;
 
-    if (spam == 'y' || spam == 'Y') {
+    if (spam == 'y' or spam == 'Y') {
         srand(static_cast<unsigned int>(time(nullptr)));
-        for (long int i = 999; i > 1; i--) {
+        for (long int i = option; i > 1; i--) {
             long int random_input = ((size / 10) * (1 + rand() % (8))) + ((rand() % (i + 1)) * (990));
             numbers->set(random_input, 1);
         }
@@ -51,12 +53,11 @@ void Task2()
     auto end_time = high_resolution_clock::now();
     auto exec_time = duration_cast<milliseconds>(end_time - start_time);
     outfile.close();
-    cout << "The sorting result was written to a file.\n";
+    cout << "The sorting result was written to a file." << endl;
     cout << "Time spent on execution " << exec_time.count() << " milliseconds." << endl;
 }
 
 int main() {
-    setlocale(LC_ALL, "RUS");
     Task2();
     return 0;
 }
